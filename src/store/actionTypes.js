edde44dd67fd4174dd197ceecb5305503f4b36a1
Tokenfire <3550505854@qq.com>
2019-06-27 10:59:13 +0800

    change

---
+++ src/store/actionTypes.js
@@ -0,0 +1,4 @@
+ const CHANGE_INPUT_VALUE="change_input_value";
+ const ADD_TODO_ITEM="add_todo_item";
+ const HANDLE_DELETE_ITEM ="handle_delete_item";
+export {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,HANDLE_DELETE_ITEM};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,4 @@
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,HANDLE_DELETE_ITEM} from "./actionTypes";
 const defaultState={
  inputValue:"iverson",
  list:[ 
@@ -8,20 +9,24 @@ const defaultState={
  'Los Angeles battles huge wildfires.',]
 }
 export default (state=defaultState,action)=>{
-    if(action.type==="change_input_value"){
+    if(action.type===CHANGE_INPUT_VALUE){
         const newState=JSON.parse(JSON.stringify(state)); // 1* 深拷贝对象 
         newState.inputValue=action.value;
         console.log(action);
         return newState;
     }
-    if(action.type==="add_todo_item"){
+    if(action.type===ADD_TODO_ITEM){
         const newState=JSON.parse(JSON.stringify(state));
          newState.list.push(newState.inputValue);
          newState.inputValue="";
          console.log(newState);
          return newState;
     }
-       
+    if(action.type===HANDLE_DELETE_ITEM){
+        const newState=JSON.parse(JSON.stringify(state));
+        newState.list.splice(action.index,1);
+        return newState;
+    } 
     console.log(action);
     return state;
 }
